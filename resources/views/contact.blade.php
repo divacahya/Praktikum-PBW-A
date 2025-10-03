9c17fce7c7826fad772053135d28e0dfeb8934a9
divacahya <spodiva@gmail.com>
2025-10-03 13:02:42 +0700

    First commit - LaraPress project

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Kontak Kami - LaraPress</title>
+</head>
+<body>
+    <h1>Kontak LaraPress</h1>
+    <p>Hubungi Kami segera</p>
+    
+    <h2>Informasi Kontak</h2>
+    <p>Jika Anda memiliki pertanyaan atau ingin bekerja sama, silakan hubungi kami melalui salah satu cara berikut:</p>
+    
+    <ul>
+        <li><strong>Telepon  :</strong> <a href="tel: +62211234567">+62 21 1234 567</a></li>
+        <li><strong>WhatsApp :</strong> <a href="https://wa.me/628123456789">+62 81 2345 6789</a></li>
+        <li><strong>Email    :</strong> <a href="mailto:kontak@larapress.id">larapres@gmail.com</a></li>
+    </ul>
+    
+    <hr>
+    <ul>
+    <li><a href="/">Halaman Utama</a></li>
+    <li><a href="/tentang-kami">Tentang Kami</a></li>
+</ul>
+</body>
+</html>
